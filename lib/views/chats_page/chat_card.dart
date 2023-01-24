3ab5f0d14770e1cc08c5b362563d3794635006c1
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-01-24 19:31:42 -0300

    estilos en chat card

--- lib/views/chats_page/chat_card.dart
+++ lib/views/chats_page/chat_card.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
 
 class ChatCard extends StatelessWidget {
-  const ChatCard({Key? key}) : super(key: key);
+  ChatCard(
+      {Key? key,
+      required this.contact,
+      required this.time,
+      required this.imageUrl,
+      required this.message})
+      : super(key: key);
+
+  final String contact;
+  String message;
+  final String time;
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+      onTap: () {},
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(imageUrl),
+        radius: 25,
+      ),
+      title: Text(contact),
+      subtitle: Text(message),
+      trailing: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(time),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/views/chats_page/chats_page.dart
+++ lib/views/chats_page/chats_page.dart
@@ -7,8 +7,8 @@ class ChatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView(
-      children: const[
-        ChatCard(),
+      children: [
+        ChatCard(contact: 'Javi ENP', time: '13:50', imageUrl: 'https://images.unsplash.com/photo-1616588945355-bb09c77baf29?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80', message: 'Hola, cómo va?',),
       ],
     );
   }
