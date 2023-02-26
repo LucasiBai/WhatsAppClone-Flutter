b2101413a0a3fdfb6270fcfce39ac01592b32bd5
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2023-02-26 11:43:16 -0300

    agregado de historias con tiempo

--- lib/src/views/status_page/status_view/status_view.dart
+++ lib/src/views/status_page/status_view/status_view.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:whatsapp_clone/src/_widgets/contact_image.dart';
+import 'package:story_view/story_view.dart';
+
 import 'package:whatsapp_clone/src/services/data_service.dart';
-import 'package:whatsapp_clone/src/services/routing_service.dart';
-import 'package:whatsapp_clone/src/styles.dart';
 import 'package:whatsapp_clone/src/views/status_page/status_view/status_view_card.dart';
 
 class StatusView extends StatefulWidget {
@@ -14,24 +13,23 @@ class StatusView extends StatefulWidget {
 }
 
 class _StatusViewState extends State<StatusView> {
-  List _urls = [];
+  final StoryController controller = StoryController();
 
-  int _currentIdx = 0;
+  List<StoryItem> _contactStories = [];
   Map _contactData = {};
 
   void getData() async {
     final data = await getStatusData(int.parse(widget.userId));
     setState(() {
-      _urls = data["stories"];
       _contactData = {"image": data["contactImg"], "name": data["contactName"]};
-    });
-  }
-
-  void nextStatus() {
-    final lastIdx = _urls.length - 1;
-
-    setState(() {
-      _currentIdx = _currentIdx < lastIdx ? _currentIdx + 1 : 0;
+      for (final url in data["stories"]) {
+        _contactStories.add(
+          StoryItem.pageImage(
+            url: url,
+            controller: controller,
+          ),
+        );
+      }
     });
   }
 
@@ -46,59 +44,11 @@ class _StatusViewState extends State<StatusView> {
     return Scaffold(
       backgroundColor: Colors.black,
       body: SafeArea(
-        child: Stack(
-          children: [
-            GestureDetector(
-              onTap: () {
-                nextStatus();
-              },
-              child: StatusViewCard(url: _urls[_currentIdx]),
-            ),
-            Align(
-              alignment: Alignment.topCenter,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                          onPressed: () {
-                            goBack(context);
-                          },
-                          icon: const Icon(
-                            AppIcons.backIcon,
-                            color: Colors.white,
-                          )),
-                      Container(
-                          decoration: BoxDecoration(
-                              borderRadius: AppBorderRadius.xlAll,
-                              color: Colors.white),
-                          padding: const EdgeInsets.all(1),
-                          margin: const EdgeInsets.fromLTRB(
-                              0, 0, AppPaddings.md, 0),
-                          child: ContactImage(
-                              onTap: () {},
-                              size: AppSizes.smSize,
-                              imageUrl: _contactData["image"])),
-                      Text(
-                        _contactData["name"],
-                        style: TextStyle(color: Colors.white),
-                      )
-                    ],
-                  ),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        AppIcons.optionsIcon,
-                        color: Colors.white,
-                      ))
-                ],
-              ),
-            )
-          ],
-        ),
-      ),
+          child: StatusViewCard(
+        stories: _contactStories,
+        controller: controller,
+        contactData: _contactData,
+      )),
     );
   }
 }
--- lib/src/views/status_page/status_view/status_view_card.dart
+++ lib/src/views/status_page/status_view/status_view_card.dart
@@ -1,19 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:story_view/story_view.dart';
+import 'package:whatsapp_clone/src/_widgets/contact_image.dart';
+import 'package:whatsapp_clone/src/services/routing_service.dart';
+import 'package:whatsapp_clone/src/styles.dart';
 
 class StatusViewCard extends StatelessWidget {
-  const StatusViewCard({Key? key, this.url}) : super(key: key);
+  StatusViewCard(
+      {Key? key,
+      required this.contactData,
+      required this.stories,
+      required this.controller})
+      : super(key: key);
 
-  final url;
+  final contactData;
+  final List<StoryItem> stories;
+  final StoryController controller;
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Expanded(
-        child: Image.network(
-          url,
-          fit: BoxFit.contain,
-        ),
-      )
-    ]);
+    return Stack(
+      children: [
+        StoryView(
+        storyItems: stories,
+        controller: controller,
+        onStoryShow: (s) {
+          print("Showing a story");
+        },
+        onComplete: () {
+          goBack(context);
+        },
+        progressPosition: ProgressPosition.top,
+        repeat: false,
+        inline: true,
+      ),
+      _getViewBar(context)
+      ]
+    );
+  }
+
+  Widget _getViewBar(BuildContext context) {
+    return Align(
+        alignment: Alignment.topCenter,
+        child: Container(
+          margin: AppPaddings.mdVer,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  IconButton(
+                      onPressed: () {
+                        goBack(context);
+                      },
+                      icon: const Icon(
+                        AppIcons.backIcon,
+                        color: Colors.white,
+                      )),
+                  Container(
+                      decoration: BoxDecoration(
+                          borderRadius: AppBorderRadius.xlAll,
+                          color: Colors.white),
+                      padding: const EdgeInsets.all(1),
+                      margin: const EdgeInsets.fromLTRB(0, 0, AppPaddings.md, 0),
+                      child: ContactImage(
+                          onTap: () {},
+                          size: AppSizes.smSize,
+                          imageUrl: contactData["image"])),
+                  Text(
+                    contactData["name"],
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    AppIcons.optionsIcon,
+                    color: Colors.white,
+                  ))
+            ],
+          ),
+        ));
   }
 }
